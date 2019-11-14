98c955fd3ed739b7ad0b6d58b4a0149e09ad5719
김창원 <ck5005v@gmail.com>
2019-11-14 14:02:20 +0900

    MoveSpeedItem
    change

--- BomberMan/Object/MapObject/Item/ItemMoveSpeed.cs
+++ BomberMan/Object/MapObject/Item/ItemMoveSpeed.cs
@@ -3,7 +3,7 @@ class ItemMoveSpeed : ItemBase
 {
     public ItemMoveSpeed()
     {
-        _image = '킥';
+        _image = '스';
     }
 
     public override MAPOBJECT_NAME Name() { return MAPOBJECT_NAME.MOVE_SPEED; }
